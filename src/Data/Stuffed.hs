e4619c35cf6783a7ea5f6992c54ebf8dfc4b5d86
Dario Bertini <berdario@gmail.com>
2017-06-11 21:36:40 +0100

    First Lazy ByteString implementation

---
+++ src/Data/Stuffed.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeInType #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GADTs #-}
+
+module Data.Stuffed
+    ( Stuffed
+    , stuff
+    , unstuff
+    , unwrap
+    )
+    where
+
+import Data.ByteString.Lazy (ByteString, split, length, uncons, splitAt, null)
+import Data.ByteString.Builder (Builder, word8, shortByteString, lazyByteString, toLazyByteString)
+import Data.ByteString.Short (toShort)
+import Data.Int (Int64)
+import Data.Proxy (Proxy (..))
+import Data.Monoid ((<>))
+import Data.Word (Word8)
+import Data.Reflection (Reifies, reflect, reifyNat)
+import GHC.TypeLits (KnownNat, CmpNat)
+import GHC.Types (Nat)
+import Prelude hiding (null, length, splitAt)
+
+
+newtype Stuffed (a :: Nat) = Stuffed ByteString
+    deriving (Eq, Ord, Show, Monoid)
+
+stuff :: forall a. (KnownNat a, CmpNat a 256 ~ 'LT) => ByteString -> Stuffed a
+stuff bs = Stuffed $ toLazyByteString $ mconcat chunks
+    where
+        excluded = fromIntegral (reflect (Proxy :: Proxy a) :: Integer)
+        chunks = map (buildStuffed excluded) $ splitEvery 254 bs
+
+splitEvery :: Int64 -> ByteString -> [ByteString]
+splitEvery n "" = []
+splitEvery n bs = start : splitEvery n rest
+    where
+        (start, rest) = splitAt n bs
+
+buildStuffed :: Word8 -> ByteString -> Builder
+buildStuffed w bs = foldl (\b a -> b <> word8 (offset a) <> lazyByteString a) mempty chunks
+    where
+        chunks = split w bs
+        offset = fromOffset w . length
+
+fromOffset :: Word8 -> Int64 -> Word8
+fromOffset excluded n = fromIntegral n + 1 + excluded
+
+toOffset :: Word8 -> Word8 -> Int64
+toOffset excluded b = fromIntegral $ b - 1 - excluded
+
+unstuff :: forall a. (KnownNat a) => Stuffed a -> ByteString
+unstuff (Stuffed bs) = toLazyByteString $ mconcat chunks
+    where
+        excluded = fromIntegral (reflect (Proxy :: Proxy a) :: Integer)
+        rebuild bs = rebuildChunk excluded (uncons bs) mempty
+        chunks = map rebuild $ splitEvery 255 bs
+
+rebuildChunk :: Word8 -> Maybe (Word8, ByteString) -> Builder -> Builder
+rebuildChunk _ Nothing b = b
+rebuildChunk excluded (Just (ovhd, rest)) b = rebuildChunk excluded (uncons after) $ b <> lazyByteString before <> nextExcluded
+    where
+        (before, after) = splitAt (toOffset excluded ovhd) rest
+        nextExcluded = if null after then mempty else word8 excluded
+
+
+unwrap :: Stuffed a -> ByteString
+unwrap (Stuffed bs) = bs
+
---
+++ test/Spec.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeInType #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ConstraintKinds #-}
+
+import Control.Monad (unless)
+import Control.Monad.Identity (Identity)
+import Control.Monad.Morph    (generalize, hoist)
+import Data.ByteString.Lazy (ByteString, length, notElem, fromStrict)
+import Data.Proxy (Proxy(..))
+import Data.Reflection (Reifies, reflect, reifyNat)
+import Data.Word (Word8)
+import GHC.Exts (Constraint)
+import GHC.TypeLits (KnownNat, CmpNat)
+import Hedgehog
+import Hedgehog.Gen (bytes, word8)
+import Hedgehog.Range (constant, constantBounded)
+import Prelude hiding (length, notElem)
+import System.Exit (exitFailure)
+import Unsafe.Coerce (unsafeCoerce)
+import System.IO.Unsafe
+
+import Data.Stuffed (Stuffed, stuff, unstuff, unwrap)
+
+data Dict (c :: Constraint) where
+    Dict :: (c) => Dict c
+
+main :: IO ()
+main = do
+    result <- checkParallel $ Group "Stuffed properties"
+                    [ ("roundtrips",
+                        stuffedProperty (bytesAndExclusion 0) (const roundTrips))
+                    , ("1 byte bigger every 254",
+                        withTests 1000 $
+                        stuffedProperty (bytesAndExclusion 1) (const biggerBy1Every254))
+                    , ("Doesn't contain the excluded byte",
+                        stuffedProperty (bytesAndExclusion 0) notContains) ]
+    unless result exitFailure
+
+type Generator = Gen Identity (ByteString, Word8)
+
+bytesAndExclusion :: Int -> Generator
+bytesAndExclusion minSize = (,) <$> (fromStrict <$> bytes (constant minSize 1100)) <*> word8 constantBounded
+
+smallerThan256 :: forall a. (KnownNat a) => Maybe (Dict (KnownNat a, CmpNat a 256 ~ 'LT))
+smallerThan256 = if (n < 256)
+        then Just $ unsafeCoerce (Dict :: Dict (KnownNat a, 'LT ~ 'LT))
+        else Nothing
+    where
+        n = reflect (Proxy :: Proxy a)
+
+stuffedProperty :: Generator -> (forall a. (KnownNat a, CmpNat a 256 ~ 'LT) => Word8 -> ByteString -> Proxy a -> Test Identity ()) -> Property
+stuffedProperty gen prop = property $ hoist generalize $ do
+    (x, w) <- forAll gen
+    reifyNat (fromIntegral w) $ \(p :: Proxy n) ->
+        case smallerThan256 of
+            Nothing -> failure
+            (Just (Dict :: Dict (KnownNat n, CmpNat n 256 ~ 'LT))) -> (prop w x p)
+
+
+
+roundTrips :: forall a. (KnownNat a, CmpNat a 256 ~ 'LT) => ByteString -> Proxy a -> Test Identity ()
+roundTrips bs _ = bs === unstuff (stuff bs :: Stuffed a)
+
+biggerBy1Every254 :: forall a. (KnownNat a, CmpNat a 256 ~ 'LT) => ByteString -> Proxy a -> Test Identity ()
+biggerBy1Every254 bs _ = length stf === inputL + (inputL) `div` 255 + 1
+    where
+        stf = unwrap (stuff bs :: Stuffed a)
+        inputL = length bs
+
+
+notContains :: forall a. (KnownNat a, CmpNat a 256 ~ 'LT) => Word8 -> ByteString -> Proxy a -> Test Identity ()
+notContains i bs _ = assert $ i `notElem` stf
+    where
+        stf = unwrap (stuff bs :: Stuffed a)
+
